d58a1c0493b87f7bd4f0a8a029dce4bfe1434078
Furkan Hanci <furkanmediastudios@gmail.com>
2022-06-13 11:51:46 +0300

    DataRequest made more clean

---
+++ DataProvider/DataProvider/Requests/FetchData/GameDataRequest.swift
@@ -0,0 +1,19 @@
+//
+//  GameDataRequest.swift
+//  DataProvider
+//
+//  Created by Furkan Hanci on 6/13/22.
+//
+
+public struct GameDataRequest: RequestProtocol {
+    
+    public typealias ResponseType = <#T##Type###>
+    
+    public var path: String = <#T##Type###>
+    public var method: RequestMethod = <#T##Type###>
+    public var parameters: RequestParameters = [:]
+    public var headers: RequestHeaders = [:]
+    
+    public init() {}
+    
+}
