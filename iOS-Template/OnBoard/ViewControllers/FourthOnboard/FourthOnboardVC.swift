616ea48ed1a8d4863229ec8d5e9bee93d8d7db27
Furkan Hanci <furkanmediastudios@gmail.com>
2022-06-14 15:55:50 +0300

    Bug fix in the home screen nav bar

--- iOS-Template/OnBoard/ViewControllers/FourthOnboard/FourthOnboardVC.swift
+++ iOS-Template/OnBoard/ViewControllers/FourthOnboard/FourthOnboardVC.swift
@@ -55,10 +55,11 @@ class FourthOnboardVC: UIViewController {
     
     @objc private func startButtonHandle(_ sender: UIButton) {
         //TODO: MIMARIYE UYGUN YAP AMK 😡
-        let cleanNavigation = CleanNavigationController(rootViewController: MainTabBarController())
-        cleanNavigation.modalTransitionStyle = .partialCurl
-        cleanNavigation.modalPresentationStyle = .fullScreen
-        present(cleanNavigation, animated: true, completion: nil)
+        let tabBar = MainTabBarController()
+       // let cleanNavigation = CleanNavigationController(rootViewController: MainTabBarController())
+        tabBar.modalTransitionStyle = .partialCurl
+        tabBar.modalPresentationStyle = .fullScreen
+        present(tabBar, animated: true, completion: nil)
         DefaultsKey.isFirstRun.value = false
     }
     
