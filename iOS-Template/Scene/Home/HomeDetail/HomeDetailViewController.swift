9f782c1d0655242d346243f6db84038f3697e106
Furkan Hanci <furkanmediastudios@gmail.com>
2022-07-19 19:53:14 +0300

    Added image to detail screen (it's bigger than cell image)

--- iOS-Template/Scene/Home/HomeDetail/HomeDetailViewController.swift
+++ iOS-Template/Scene/Home/HomeDetail/HomeDetailViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 import MobilliumBuilders
 import TinyConstraints
+import Kingfisher
 final class HomeDetailViewController: BaseViewController<HomeDetailViewModel> {
+    
+    private let detailImage = UIImageViewBuilder()
+        .size(CGSize(width: UIScreen.main.bounds.size.width, height: 210))
+        .cornerRadius(35)
+        .build()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureContents()
+        addSubViews()
         
     }
 }
@@ -22,7 +29,13 @@ final class HomeDetailViewController: BaseViewController<HomeDetailViewModel> {
 extension HomeDetailViewController {
     
     private func addSubViews() {
-       
+       addDetailImage()
+        addDetailImage()
+    }
+    
+    private func addDetailImage() {
+        view.addSubview(detailImage)
+        detailImage.topToSuperview(view.readableContentGuide.topAnchor , offset: -32)
     }
     
 }
@@ -31,12 +44,19 @@ extension HomeDetailViewController {
 extension HomeDetailViewController {
     private func configureContents() {
         addBarButton()
+        configureDetailElements()
     }
     
     private func addBarButton() {
         let navBarButton = UIBarButtonItem(image: AppConstants.Style.Image.upload, style: .plain, target: self, action: #selector(shareButtonHandle(_:)))
         navBarButton.tintColor = AppConstants.Style.Color.labelColor
         self.navigationItem.rightBarButtonItem = navBarButton
+    }
+    
+    private func configureDetailElements() {
+        let imageUrl = URL(string: viewModel.model.image ?? "")
+        detailImage.kf.setImage(with: imageUrl  ,options: [.scaleFactor(UIScreen.main.scale), .transition(.fade(1))])
+        
     }
 }
 
