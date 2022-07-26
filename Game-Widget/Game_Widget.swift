48d42f9b9d75190de5ae9b887a1656d1df41c749
Furkan Hanci <furkanmediastudios@gmail.com>
2022-07-26 20:02:55 +0300

    Widget files created

---
+++ Game-Widget/Game_Widget.swift
@@ -0,0 +1,67 @@
+//
+//  Game_Widget.swift
+//  Game-Widget
+//
+//  Created by Furkan Hanci on 7/26/22.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct Game_WidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct Game_Widget: Widget {
+    let kind: String = "Game_Widget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            Game_WidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Gamella Widget 🚀")
+        .description("Power Widgets")
+    }
+}
+
+struct Game_Widget_Previews: PreviewProvider {
+    static var previews: some View {
+        Game_WidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
